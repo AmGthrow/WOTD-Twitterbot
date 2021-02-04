74685385a27cdc1d7a5702dbeabfc1eac9ce8d21
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-04 18:17:42 +0800

    using select_one instead of select()[0]

--- wordOfTheDay.py
+++ wordOfTheDay.py
@@ -6,14 +6,15 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 
 
-message = f'''{soup.select('span.w-a-title.margin-lr-0.margin-tb-1875em')[0].text.split(':')[1][1:]}
+message = f'''{soup.select_one('.w-a-title').text.split(':')[1][1:]} 
 
-{soup.select('div.word-and-pronunciation h1')[0].text.upper()}
-{soup.select('span.main-attr')[0].text} | {soup.select('span.word-syllables')[0].text}
+{soup.h1.text.upper()} 
+{soup.select_one('.main-attr').text} | {soup.select_one('.word-syllables').text}
 
-{soup.select('div.wod-definition-container h2')[0].text.capitalize()}
+{soup.select_one('.wod-definition-container h2').text.capitalize()}
 '''
-definitionList = soup.select('div.wod-definition-container > p')
+
+definitionList = soup.select('.wod-definition-container > p')
 for definition in definitionList:
     message = message + '\n' + definition.text
 
